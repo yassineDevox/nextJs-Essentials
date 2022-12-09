f2d650f7d8ed49c2eda132fc793098b1d87a929c
Yassine Rassy <yassinDevox>
2022-12-09 16:31:32 +0100

    done

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link"
 
-
+//link component
+interface LinkItemProps { path: string; label: string }
+const LinkItem = ({ path, label }: LinkItemProps) => {
+  return <li> <Link href={path}>{label}</Link></li>
+}
+//home page 
 const HomePage = () => {
   return (
-    <> Hi Guys 😇</>
+    <ul>
+        <LinkItem label="Home" path="/"/>
+        <LinkItem label="About" path="/about"/>
+        <LinkItem label="My Profile " path="/profil"/>
+    </ul>
   )
 }
 export default HomePage
